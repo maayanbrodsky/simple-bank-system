97317313b067af2debf8cbc437b4bd8f8f7c16a4
Maayan Brodsky <maayanbrodsky@gmail.com>
2021-04-25 18:04:52 -0400

    first stage complete

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,7 +1,23 @@
 from random import randint
 
+
+ACCOUNTS = {}
+
+MAIN_MENU = ("""1. Create an account
+2. Log into account
+0. Exit\n""")
+
+ACCOUNT_MENU = ("""1. Balance
+2. Log out
+0. Exit\n""")
+
+
 class Account:
     def __init__(self, card, pin, balance=0):
+        self.card = card
+        self.pin = pin
+        self.balance = balance
+        self.card_num = card_num(card)
 
 
 class Card:
@@ -11,21 +27,14 @@ class Card:
         self.checksum = checksum
 
 
-def print_menu():
-    print("""1. Create an account
-    2. Log into account
-    0. Exit""")
-
-
-def print_card(card):
+def card_num(card):
     card_num = card.iin + card.can + card.checksum
-    print(card_num)
+    return card_num
 
 
 def gen_card():
     can = str(randint(100000000, 999999999))
     card = Card(can)
-    # print_card(card)
     return card
 
 
@@ -37,9 +46,61 @@ def gen_pin():
 def gen_account():
     card = gen_card()
     pin = gen_pin()
-#
-# choice = int(input("Please choose option: "))
-#
-# if choice is 1
+    account = Account(card, pin)
+    print(f"""Your card has been created
+Your card number:
+{account.card_num}
+Your card PIN:
+{account.pin}\n""")
+    return account
 
-print(gen_card())
+
+def login():
+    card_number = input("Enter your card number: ")
+    choice = None
+    pin = input("Enter you PIN: ")
+    if card_number in ACCOUNTS and ACCOUNTS[card_number].pin == pin:
+        print("You have successfully logged in!")
+        choice = account_menu(ACCOUNTS[card_number])
+    else:
+        print("Wrong card number or PIN!\n")
+    return choice
+
+def bank_main(choice):
+    while choice != 0:
+        print(MAIN_MENU)
+        choice = int(input())
+        if choice == 1:
+            account = gen_account()
+            ACCOUNTS[str(account.card_num)] = account
+        elif choice == 2:
+            choice = login()
+        return choice
+
+
+def account_menu(account):
+    print(ACCOUNT_MENU)
+    choice = int(input())
+    while choice == 1:
+        print(f'Balance: {account.balance}')
+        print(ACCOUNT_MENU)
+        choice = int(input())
+    if choice == 2:
+        print("You have successfully logged out!")
+
+        return None
+    if choice == 0:
+        return 0
+
+
+choice = None
+while choice != 0:
+    choice = bank_main(choice)
+print("Bye!")
+
+
+
+
+
+
+
